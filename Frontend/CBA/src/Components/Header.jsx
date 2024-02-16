6da44329f0b0c669eab4d7f587349a223a8b83f9
Osy <ikuenobeosi@gmail.com>
2024-02-16 02:32:21 +0100

    Removed unused imports and added logout to Header

--- Frontend/CBA/src/Components/Header.jsx
+++ Frontend/CBA/src/Components/Header.jsx
@@ -1,69 +1,116 @@
-import { useState } from 'react';
-import { styled} from '@mui/material/styles';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Button from "@mui/material/Button";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
-import { DRAWER_WIDTH } from '../constants';
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
+import { DRAWER_WIDTH, TOAST_CONFIG } from "../Util/constants";
+import { logoutUser, clearTokenData } from "../Util/auth";
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     marginLeft: DRAWER_WIDTH,
     width: `calc(100% - ${DRAWER_WIDTH}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
+export default function Header({ sideBarOpen, toggleSideBar }) {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
-export default function Header({open, toggleDrawer}){
-  return (<AppBar position="absolute" open={open}>
-  <Toolbar
-    sx={{
-      pr: '24px', // keep right padding when drawer closed
-    }}
-  >
-    <IconButton
-      edge="start"
-      color="inherit"
-      aria-label="open drawer"
-      onClick={toggleDrawer}
-      sx={{
-        marginRight: '36px',
-        ...(open && { display: 'none' }),
-      }}
-    >
-      <MenuIcon />
-    </IconButton>
-    <Typography
-      component="h1"
-      variant="h6"
-      color="inherit"
-      noWrap
-      sx={{ flexGrow: 1 }}
-    >
-      Dashboard
-    </Typography>
-    <IconButton color="inherit">
-      <Badge badgeContent={4} color="secondary">
-        <NotificationsIcon />
-      </Badge>
-    </IconButton>
-  </Toolbar>
-</AppBar>)
+  const logOut = (event) => {
+    event.preventDefault();
+    setIsLoading(true)
+    logoutUser()
+      .then((data) => {
+        console.log(data);
+        clearTokenData();
+        if (!data.ok || data.error)
+          throw new Error(data.message || data.error);
+
+        clearTokenData();
+        setIsLoading(false)
+        toast.success("Successfull", TOAST_CONFIG);
+        navigate("/login");
+      })
+      .catch((error) => {
+        setIsLoading(false)
+        toast.error(error.message, TOAST_CONFIG);
+      });
+  };
+
+  const viewProfile = () => {};
+
+  return (
+    <AppBar position="absolute" open={sideBarOpen}>
+      <Toolbar
+        sx={{
+          pr: "24px", // keep right padding when drawer closed
+        }}
+      >
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={toggleSideBar}
+          sx={{
+            marginRight: "36px",
+            ...(sideBarOpen && { display: "none" }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{ flexGrow: 1 }}
+        >
+          Dashboard
+        </Typography>
+        <Button
+          variant="text"
+          color="inherit"
+          startIcon={<LogoutIcon />}
+          sx={{ ml: 1 }}
+          onClick={logOut}
+        >
+          LogOut
+        </Button>
+      </Toolbar>
+      <Backdrop
+            sx={{
+              color: "#fff",
+              zIndex: (theme) => theme.zIndex.drawer + 1,
+            }}
+            open={isLoading}
+          >
+            <CircularProgress color="inherit" />
+          </Backdrop>
+    </AppBar>
+  );
 }
