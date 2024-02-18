2deb21b9ad0992b1070d8fa40fb6466b69141da9
Osy <ikuenobeosi@gmail.com>
2024-02-18 22:24:50 +0100

    Removed unused imports

--- Frontend/CBA/src/Pages/Dashboard.jsx
+++ Frontend/CBA/src/Pages/Dashboard.jsx
@@ -1,32 +1,12 @@
 import { useState } from "react";
-import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import Badge from "@mui/material/Badge";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Link from "@mui/material/Link";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import NotificationsIcon from "@mui/icons-material/Notifications";
 import { Outlet } from "react-router-dom";
 import SideBar from "../Components/SideBar";
-import {
-  Link as RouterLink,
-  useNavigate,
-  redirect,
-  Navigate,
-} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-import { DRAWER_WIDTH } from "../utils/constants";
 import Header from "../Components/Header";
-import { tokenExists } from "../api/auth";
+import { tokenExists } from "../utils/token";
 import Copyright from "../Components/Copyright";
 
 export default function Dashboard(props) {
