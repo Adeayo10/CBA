91fefd9c98f1dcc4e86d7b6397e23447a653bb7b
Osy <ikuenobeosi@gmail.com>
2024-02-18 22:09:35 +0100

    moved token logic from api/auth to utils/token

---
+++ Frontend/CBA/src/utils/token.js
@@ -0,0 +1,67 @@
+import Cookies from "universal-cookie";
+import { jwtDecode } from "jwt-decode";
+
+const cookies = new Cookies();
+
+export function getCurrentRole() {
+  //todo implement role fetching logic
+  return "SuperAdmin";
+}
+
+export function saveTokenData(accessToken, refreshToken, expiryDate) {
+  cookies.set("accessToken", accessToken, {
+    path: "/",
+    secure: true,
+    sameSite: false,
+  });
+
+  cookies.set("refreshToken", refreshToken, {
+    path: "/",
+    secure: true,
+    sameSite: false,
+  });
+
+  cookies.set("expiryDate", expiryDate, {
+    path: "/",
+    secure: true,
+    sameSite: false,
+  });
+}
+
+export function clearTokenData() {
+  cookies.remove("accessToken", { path: "/" });
+  cookies.remove("refreshToken", { path: "/" });
+  cookies.remove("expiryDate", { path: "/" });
+}
+
+export function tokenExists() {
+  return Boolean(retrieveAccessToken());
+}
+
+export function tokenExpired() {
+  const expiryDate = retrieveExpiryDate();
+  return new Date().getTime() > new Date(expiryDate).getTime();
+}
+
+export function redirectIfRefreshTokenExpired(errorMessage, navigate) {
+  if (!errorMessage.toLowerCase().includes("expired")) return;
+  clearTokenData();
+  navigate("/login");
+}
+
+export function retrieveRefreshToken() {
+  return cookies.get("refreshToken");
+}
+
+export function retrieveAccessToken() {
+  return cookies.get("accessToken");
+}
+
+export function retrieveUserFromToken() {
+  const { role, email, unique_name } = jwtDecode(retrieveAccessToken());
+  return { role, email, username: unique_name };
+}
+
+function retrieveExpiryDate() {
+  return cookies.get("expiryDate");
+}
