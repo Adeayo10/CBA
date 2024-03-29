503c996f222235941f0a706480e64a28db654eb9
Adeayo <aadelee74@gmail.com>
2024-03-29 11:59:33 +0100

    Modify CustomerEntity migration to change AccountType column type

---
+++ Migrations/20240328024444_modifiedCustomerEntity.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CBA.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifiedCustomerEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountType",
+                table: "CustomerEntity",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountType",
+                table: "CustomerEntity",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
