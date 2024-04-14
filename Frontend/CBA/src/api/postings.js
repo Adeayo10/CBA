d322d22fa819b76ec4b91809d4f2b40e96b07437
Osy <ikuenobeosi@gmail.com>
2024-04-14 23:09:42 +0100

    Implemented API method to get all postings

--- Frontend/CBA/src/api/postings.js
+++ Frontend/CBA/src/api/postings.js
@@ -41,7 +41,7 @@ export async function createWithdrawal(withdrawalPosting) {
 export async function createDeposit(depositPosting) {
   const API_URL = `/api/v1/Posting/Deposit`;
 
-  console.log({depositPosting})
+  console.log({ depositPosting });
   if (tokenExpired()) await refreshAccessToken();
 
   const headers = {
@@ -51,7 +51,28 @@ export async function createDeposit(depositPosting) {
   const response = await fetch(API_URL, {
     method: "POST",
     headers,
-    body: JSON.stringify(depositPosting)
+    body: JSON.stringify(depositPosting),
+  });
+  return await response.json();
+}
+
+export async function getPostings(
+  postingType,
+  pageNumber = 1,
+  pageSize = PAGE_SIZE
+) {
+  const API_URL = `/api/v1/Posting/GetPostings?pageNumber=${pageNumber}&pageSize=${pageSize}&filterValue=${postingType}`;
+
+  if (tokenExpired()) await refreshAccessToken();
+
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: getAuthorizationHeader(),
+  };
+  //console.log(limit);
+  const response = await fetch(API_URL, {
+    method: "GET",
+    headers,
   });
   return await response.json();
 }
